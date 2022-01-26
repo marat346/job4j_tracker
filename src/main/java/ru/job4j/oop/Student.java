9a5ba38c846b115361ee5e5ff4d7e234f19ffeb8
marat346 <dela346@mail.ru>
2022-01-26 20:43:18 +0500

    1.1. Создание объекта [#165452 #255268]

--- src/main/java/ru/job4j/oop/Student.java
+++ src/main/java/ru/job4j/oop/Student.java
@@ -7,7 +7,7 @@ public class Student {
     }
 
     public void song() {
-        System.out.println("I believe I can fly");
+        System.out.println("I believe I man fly");
     }
 
     public static void main(String[] args) {
