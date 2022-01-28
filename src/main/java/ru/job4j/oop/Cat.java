1ddb1e751609fecb0324a20e5e404f827549e7e0
marat346 <dela346@mail.ru>
2022-01-28 22:01:46 +0500

    1.6. Взаимодействие объектов [#166087 #256621]

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -19,15 +19,25 @@ package ru.job4j.oop;
         }
 
         public static void main(String[] args) {
+
         System.out.println("There are gav's food.");
+
         Cat gav = new Cat();
+
         gav.eat("kotleta");
+
         gav.giveNick("Liza");
+
         gav.show();
+
         System.out.println("There are black's food.");
+
         Cat black = new Cat();
+
         black.eat("fish");
+
         black.giveNick("Voron");
+
         black.show();
         }
  }
