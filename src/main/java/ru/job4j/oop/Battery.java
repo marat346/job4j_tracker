80974c81164373e8227de1f588343781404fe1b6
marat346 <dela346@mail.ru>
2022-02-07 12:56:50 +0500

    1.8. Состояние объекта [#167388 #261000]

--- src/main/java/ru/job4j/oop/Battery.java
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -12,8 +12,8 @@ public class Battery {
     }
 
     public void exchange(Battery another) {
-        this.load += another.load;
-
+        another.load += this.load;
+        this.load = 0;
     }
 }
 
