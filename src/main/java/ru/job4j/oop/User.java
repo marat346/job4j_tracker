dfb579cb38bb190f718b27cec16854dd85c7a6bd
marat346 <dela346@mail.ru>
2022-03-01 15:58:12 +0500

    5. Локальные переменные и поля. [#180148]

--- src/main/java/ru/job4j/oop/User.java
+++ src/main/java/ru/job4j/oop/User.java
@@ -2,9 +2,9 @@ package ru.job4j.oop;
 
 public class User {
 
-    private String name;// поле объекта //
+    private String name; // поле объекта //
 
-    private int age;// поле объекта //
+    private int age; // поле объекта //
 
     public boolean canDrive() {
         boolean can = false; // локальная переменная//
