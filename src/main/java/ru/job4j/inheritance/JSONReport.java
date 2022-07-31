e672a38e9efbbd7a57b9d4fa2ead599fe2c49d49
marat346 <dela346@mail.ru>
2022-07-31 12:25:49 +0500

    7. Date. Отображение даты

--- src/main/java/ru/job4j/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -2,8 +2,16 @@ package ru.job4j.inheritance;
 
 public class JSONReport extends TextReport {
     public String generate(String name, String body) {
-        return "{" + System.lineSeparator() + name
-                + "name" + System.lineSeparator() + body
-                + "body" + System.lineSeparator() + "}";
+        return "{" + System.lineSeparator()
+                + "name" + ":" + name + "," + System.lineSeparator()
+                + "body" + ":" + body + "," + System.lineSeparator()
+                + "}";
+
+    }
+
+    public static void main(String[] args) {
+        JSONReport report = new JSONReport();
+        String text = report.generate("Report's name", "Report's body");
+        System.out.println(text);
     }
 }
