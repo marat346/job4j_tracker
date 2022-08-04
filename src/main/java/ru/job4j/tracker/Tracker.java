d4d66bebf45070fce81ed3e96a53a89d6a15ab44
marat346 <dela346@mail.ru>
2022-08-05 00:31:55 +0500

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,17 +1,54 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
     private int size = 0;
+    private String key = name.getName();
 
-    public Item add(Item item) {
+    public Item add(Item item) {/// добавление заявок///
         item.setId(ids++);
         items[size++] = item;
         return item;
     }
 
-    public Item findById(int id) {
+    public Item[] findAll() {/// получение списка всех заявок ////
+        Item[] result = new Item[items.length];
+        for (int index = 0; index < items.length; index++) {
+            Item tot = items[index];
+            if (tot != null) {
+                items[size] = tot;
+                size++;
+            }
+        }
+        result = Arrays.copyOf(result, size);
+        for (int index = 0; index < result.length; index++) {
+            System.out.println(result[index]);
+        }
+        return result;
+    }
+
+    public Item[] findByName(String key) {////получение списка по имени/////
+        Item[] name = new Item[items.length];
+        Item[] rsl = new Item[name.length];
+        int size = 0;
+        for (int index = 0; index < rsl.length; index++) {
+            Item clue = rsl[index];
+            if (key.equals(clue.getName())) {
+                rsl[size] = clue;
+                size++;
+            }
+        }
+        rsl = Arrays.copyOf(rsl, size);
+        for (int index = 0; index < rsl.length; index++) {
+            System.out.println(rsl[index]);
+        }
+        return rsl;
+    }
+
+    public Item findById(int id) { /// получение заявки ////
         Item rsl = null;
         for (int index = 0; index < size; index++) {
             Item item = items[index];
