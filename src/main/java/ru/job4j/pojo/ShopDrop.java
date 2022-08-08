4c508ee4c5225a7f2c27df7834be7c2e9ec9227a
marat346 <dela346@mail.ru>
2022-08-08 22:12:14 +0500

    3. Массивы и модели. [#174892 #328612]

--- src/main/java/ru/job4j/pojo/ShopDrop.java
+++ src/main/java/ru/job4j/pojo/ShopDrop.java
@@ -4,14 +4,10 @@ public class ShopDrop {
     public static Product[] delete(Product[] products, int index) {
         products[1] = products[2];
         products[2] = null;
-        for (int i = 0; i < products.length; i++) {
-            Product product = products[i];
-            if (product != null) {
-                System.out.println(product.getName());
-            } else {
-                System.out.println("null");
+        for (int i = index; i < products.length - 1; i++) {
+            products[i] = products[i + 1];
+            products[products.length - 1] = null;
             }
-        }
         return products;
     }
 }
