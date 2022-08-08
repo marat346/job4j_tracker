d423057b4716bea82a54a65460a6be45743b7295
marat346 <dela346@mail.ru>
2022-08-08 22:16:36 +0500

    3. Массивы и модели. [#174892 #328612]

--- src/test/java/ru/job4j/oop/TriangleTest.java
+++ src/test/java/ru/job4j/oop/TriangleTest.java
@@ -1,9 +1,9 @@
 package ru.job4j.oop;
 
-import static org.hamcrest.Matchers.closeTo;
-import static org.junit.Assert.assertThat;
-
 import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.closeTo;
 
 public class TriangleTest {
 
