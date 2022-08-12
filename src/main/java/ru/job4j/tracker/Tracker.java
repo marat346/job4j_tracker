e97745a9d525d3d03af88e40b5d6581bce1c8fbe
marat346 <dela346@mail.ru>
2022-08-12 13:41:21 +0500

    6. Метод замены заявки. Tracker.replace [#211748 #331074]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -56,7 +56,7 @@ public class Tracker {
         return result;
     }
 
-    public boolean delete(int id) {
+    public boolean delete (int id) {
         int index = indexOf(id);
         boolean result = index != -1;
         if (result) {
