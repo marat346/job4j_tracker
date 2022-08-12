bd7e658e4d5d3b8d3528a699c633ea6571972c50
marat346 <dela346@mail.ru>
2022-08-12 15:00:34 +0500

    8. Что такое валидация? [#246864]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -48,6 +48,7 @@ public class Tracker {
 
     public boolean replace(int id, Item item) {
         int index = indexOf(id);
+
         boolean result = index != -1;
         if (result) {
             item.setId(id);
@@ -58,6 +59,7 @@ public class Tracker {
 
     public boolean delete(int id) {
         int index = indexOf(id);
+
         boolean result = index != -1;
         if (result) {
             System.arraycopy(items, index + 1, items, index, size - index - 1);
