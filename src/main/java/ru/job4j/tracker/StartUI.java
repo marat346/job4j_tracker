2fe48c69f2734ba24d019e39abab863a3c600ad6
marat346 <dela346@mail.ru>
2022-08-15 23:03:26 +0500

    2.5. Реализация класса StartUI. Изменение заявки [#500746]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -17,37 +17,49 @@ public class StartUI {
                 Item item = new Item(name);
                 tracker.add(item);
                 System.out.println("Добавленная заявка: " + item);
-            } else if (select == 6) {
-                run = false;
             } else if (select == 1) {
-            System.out.println("=== Show all items ===");
-            Item[] items = tracker.findAll();
+                System.out.println("=== Show all items ===");
+                Item[] items = tracker.findAll();
                 if (items.length > 0) {
                     for (Item item : items) {
                         System.out.println(item);
                     }
                 } else {
-                    System.out.println("Хранилище еще не содержит заявок");
+                    System.out.println("Хранилище еще не содержит заявок.");
                 }
+            } else if (select == 2) {
+                System.out.println("=== Edit item ===");
+                System.out.print("Enter id: ");
+                int id = Integer.parseInt(scanner.nextLine());
+                System.out.print("Enter name: ");
+                String name = scanner.nextLine();
+                Item item = new Item(name);
+                if (tracker.replace(id, item)) {
+                    System.out.println("Заявка изменена успешно.");
+                } else {
+                    System.out.println("Ошибка замены заявки.");
+                }
+            } else if (select == 6) {
+                run = false;
             }
-          }
         }
+    }
 
         private void showMenu() {
-        String[] menu = {
-                "Add new Item", "Show all items", "Edit item",
-                "Delete item", "Find item by id", "Find items by name",
-                "Exit Program"
-        };
-        System.out.println("Menu:");
-        for (int i = 0; i < menu.length; i++) {
-            System.out.println(i + ". " + menu[i]);
+            String[] menu = {
+                    "Add new Item", "Show all items", "Edit item",
+                    "Delete item", "Find item by id", "Find items by name",
+                    "Exit Program"
+            };
+            System.out.println("Menu:");
+            for (int i = 0; i < menu.length; i++) {
+                System.out.println(i + ". " + menu[i]);
+            }
+        }
+
+        public static void main(String[]args) {
+            Scanner scanner = new Scanner(System.in);
+            Tracker tracker = new Tracker();
+            new StartUI().init(scanner, tracker);
         }
     }
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Tracker tracker = new Tracker();
-        new StartUI().init(scanner, tracker);
-    }
-}
