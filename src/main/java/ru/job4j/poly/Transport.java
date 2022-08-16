ea9b9efb4c2b4ce1a9cd78a91ae52f3af6162706
marat346 <dela346@mail.ru>
2022-08-16 14:36:47 +0500

    3. Интерфейс Input [#181092]

--- src/main/java/ru/job4j/poly/Transport.java
+++ src/main/java/ru/job4j/poly/Transport.java
@@ -2,10 +2,10 @@ package ru.job4j.poly;
 
 public interface Transport {
 
-    void rude ();
+    void rude();
 
-    void  passengers (int value);
+    void  passengers(int value);
 
-    int refuel (int fuel);
+    int refuel(int fuel);
 
 }
