593a4dcb7b0f1dfb4eabcbe22964baeb57129819
marat346 <dela346@mail.ru>
2022-08-19 20:43:40 +0500

    4.1. Разрыв зависимости StartUI от Scanner. [#181778 #333612]

--- src/main/java/ru/job4j/io/Matches.java
+++ src/main/java/ru/job4j/io/Matches.java
@@ -4,28 +4,26 @@ import java.util.Scanner;
 
 public class Matches {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Игра 11.");
+         Scanner input = new Scanner(System.in);
+         System.out.println("Игра 11.");
         boolean turn = true;
         int count = 11;
         while (count > 0) {
-            for (int i = 0; i <= count; i++) {
-                      String player = turn ? "Первый игрок" : "Второй игрок";
-                      System.out.println(player + " введите число от 1 до 3:");
-                      int matches = Integer.parseInt(input.nextLine());
-                    if (matches > 0 && matches <= 3) {
-                        count -= matches;
-                        System.out.println("Осталось" + " " + count + " " + "спичек");
-                    } else if (matches < count) {
-                        System.out.println("Неправильно вели цифру");
-                        turn = !turn;
-                    }
-                }
+         String player = turn ? "Первый игрок" : "Второй игрок";
+         System.out.println(player + " введите число от 1 до 3:");
+        int matches = Integer.parseInt(input.nextLine());
+        if (matches > 0 && matches <= Math.min(count, 3)) {
+         count -= matches;
+         System.out.println("Осталось" + " " + count + " " + "спичек");
+         turn = !turn;
+        } else {
+         System.out.println("Неправильно вели цифру");
+        }
 
-            if (!turn) {
-                System.out.println("Выиграл первый игрок");
-            } else {
-                System.out.println("Выиграл второй игрок");
+        if (!turn) {
+         System.out.println("Выиграл первый игрок");
+        } else {
+         System.out.println("Выиграл второй игрок");
             }
         }
     }
