5ff17ea415846b72009f40b2299bdb3f4f79aac0
marat346 <dela346@mail.ru>
2022-08-22 11:35:28 +0500

    5.1. Тестирование. Подготовка данных. [#182960 #333674]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -32,16 +32,15 @@ public class StartUITest {
     }
 
     @Test
-    public void deleteItem() {
+    public void whenDeleteItem() {
         Tracker tracker = new Tracker();
         Item item = new Item("null");
         tracker.add(item);
         String[] answers = {
-                String.valueOf(item.getId()),
-                "null"
+                String.valueOf(item.getId())
         };
-        StartUI.editItem(new StubInput(answers), tracker);
+        StartUI.deleteItem(new StubInput(answers), tracker);
         Item edited = tracker.findById(item.getId());
-        assertThat(edited.getName()).isEqualTo("null");
+        assertThat(edited).isNull();
     }
 }
