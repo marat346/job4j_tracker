af5d5ee213a43aaa8404aa7657fdf1f6ae83347a
marat346 <dela346@mail.ru>
2022-08-23 18:08:14 +0500

    9. Написать тесты на StartUI. [#785]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,18 +1,19 @@
 package ru.job4j.tracker;
 
 import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StartUITest {
     @Test
     public void whenCreateItem() {
         Input in = new StubInput(
-                new String[] {"0", "Item name", "1"}
+                new String[]{"0", "Item name", "1"}
         );
         Tracker tracker = new Tracker();
         UserAction[] actions = {
                 new CreateAction(),
-                new ExitProgramAction()
+                new ExitProgramAction(),
         };
         new StartUI().init(in, tracker, actions);
         assertThat(tracker.findAll()[0].getName()).isEqualTo("Item name");
@@ -21,15 +22,14 @@ public class StartUITest {
     @Test
     public void whenEditItem() {
         Tracker tracker = new Tracker();
-        new CreateAction();
-        Item item = tracker.findById(2);
-        String replacedName = "Edit item name";
+        Item item = tracker.add(new Item("Edit item"));
+        String replacedName = "New item name";
         Input in = new StubInput(
-                new String[] {"1","new", "1"}
+                new String[] {"0" , String.valueOf(item.getId()), replacedName, "1"}
         );
         UserAction[] actions = {
                 new EditAction(),
-                new ExitProgramAction()
+                new ExitProgramAction(),
         };
         new StartUI().init(in, tracker, actions);
         assertThat(tracker.findById(item.getId()).getName()).isEqualTo(replacedName);
@@ -38,15 +38,13 @@ public class StartUITest {
     @Test
     public void whenDeleteItem() {
         Tracker tracker = new Tracker();
-        /* Добавим в tracker новую заявку */
         Item item = tracker.add(new Item("Deleted item"));
-        /* Входные данные должны содержать ID добавленной заявки item.getId() */
         Input in = new StubInput(
-                new String[] {"0" /* входные параметры для DeleteAction */, "1"}
+                new String[] {"0", String.valueOf(item.getId()), "1"}
         );
         UserAction[] actions = {
                 new DeleteAction(),
-                new ExitProgramAction()
+                new ExitProgramAction(),
         };
         new StartUI().init(in, tracker, actions);
         assertThat(tracker.findById(item.getId())).isNull();
