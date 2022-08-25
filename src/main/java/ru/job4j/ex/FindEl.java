bd35d432d1210d6afb0e79b788e0d7ab6f702542
marat346 <dela346@mail.ru>
2022-08-25 10:51:55 +0500

    0.4. Пользовательские исключения. [#219367 #336070]

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -3,19 +3,18 @@ package ru.job4j.ex;
 public class FindEl {
 
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
-        if (key != null) {
-            throw new IllegalArgumentException("Key could not be less then null");
-        }
         int rsl = -1;
         for (int i = 0; i < value.length; i++) {
-            if (value[i] == key) {
+            if (value[i].equals(key)) {
                 rsl = i;
                 break;
             }
         }
         return rsl;
+        if (rsl == -1) {
+            throw new IllegalArgumentException("Key could not be less then null");
+        }
     }
-
     public static void main(String[] args) throws ElementNotFoundException {
         String[] value = {"apple", "cake", "bread"};
         String key = "cak";
