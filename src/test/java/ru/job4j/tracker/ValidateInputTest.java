8ce41e91e4235f1df03d6798a37298862ec35f8e
marat346 <dela346@mail.ru>
2022-08-31 10:58:43 +0500

    2.1. Тест на ValidateInput [#311887 #337158]

--- src/test/java/ru/job4j/tracker/ValidateInputTest.java
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -18,10 +18,10 @@ public class ValidateInputTest {
     }
 
     @Test
-    public void whenCorrectInvalidInput() {
+    public void whenValidInput() {
         Output out = new StubOutput();
         Input in = new StubInput(
-                new String[]{"1", "1"}
+                new String[]{"1"}
         );
         ValidateInput input = new ValidateInput(out, in);
         int selected = input.askInt("Enter menu:");
@@ -29,7 +29,7 @@ public class ValidateInputTest {
     }
 
     @Test
-    public void whenMultipleCorrectInvalidInput() {
+    public void whenMultipleValidInput() {
         Output out = new StubOutput();
         Input in = new StubInput(
                 new String[]{"1", "15", "17"}
@@ -44,10 +44,10 @@ public class ValidateInputTest {
     }
 
     @Test
-    public void whenMinusInvalidInput() {
+    public void whenNegativeInput() {
         Output out = new StubOutput();
         Input in = new StubInput(
-                new String[]{"-1", "1"}
+                new String[]{"-1"}
         );
         ValidateInput input = new ValidateInput(out, in);
         int selected = input.askInt("Enter menu:");
