548372aab08d7602b7f1eb83a0d04ec8d1487f1d
marat346 <dela346@mail.ru>
2022-09-01 14:46:29 +0500

    1. Singleton [#94606 #337881]

--- src/main/java/ru/job4j/tracker/SingleTracker.java
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -18,6 +18,26 @@ public class SingleTracker {
     public Item add(Item item) {
         return tracker.add(item);
     }
+
+    public Item[] findAll() {
+        return tracker.findAll();
+    }
+
+    public Item[] findByName(String key) {
+        return tracker.findByName(key);
+    }
+
+    public Item findById(int id) {
+        return tracker.findById(id);
+    }
+
+    public boolean replace(int id, Item item) {
+        return tracker.replace(id, item);
+    }
+
+    public boolean delete(int id) {
+        return tracker.delete(id);
+    }
 }
 
 
