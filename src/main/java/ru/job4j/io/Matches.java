2e363e7b5d160ca760f9a0a39c1de30ee7a31853
marat346 <dela346@mail.ru>
2022-09-05 22:33:43 +0500

    Фабрика

--- src/main/java/ru/job4j/io/Matches.java
+++ src/main/java/ru/job4j/io/Matches.java
@@ -4,29 +4,29 @@ import java.util.Scanner;
 
 public class Matches {
     public static void main(String[] args) {
-         Scanner input = new Scanner(System.in);
-         System.out.println("Игра 11.");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Игра 11.");
         boolean turn = true;
         int count = 11;
-        while (count > 0) {
-         String player = turn ? "Первый игрок" : "Второй игрок";
-         System.out.println(player + " введите число от 1 до 3:");
-        int matches = Integer.parseInt(input.nextLine());
-        if (matches > 0 && matches <= Math.min(count, 3)) {
-         count -= matches;
-         System.out.println("Осталось" + " " + count + " " + "спичек");
-         turn = !turn;
-        } else {
-         System.out.println("Неправильно вели цифру");
+        while (count > 0 && count >= 3) {
+            String player = turn ? "Первый игрок" : "Второй игрок";
+            System.out.println(player + " введите число от 1 до 3:");
+            int matches = Integer.parseInt(input.nextLine());
+            if (matches > 0 && matches <= Math.min(count, 3)) {
+                count -= matches;
+                System.out.println("Осталось" + " " + count + " " + "спичек");
+                turn = !turn;
+            } else {
+                System.out.println("Неправильно вели цифру");
+            }
         }
 
         if (!turn) {
-         System.out.println("Выиграл первый игрок");
+            System.out.println("Выиграл первый игрок");
+            System.out.println("Игра закончна");
         } else {
-         System.out.println("Выиграл второй игрок");
-            }
+            System.out.println("Выиграл второй игрок");
+            System.out.println("Игра закончна");
         }
     }
 }
-
-
