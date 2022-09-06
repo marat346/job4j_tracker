a8f5dfe829878c41205e0a0401046d24b8eb82c9
marat346 <dela346@mail.ru>
2022-09-06 14:15:00 +0500

    4. Принципы раннего возврата и охранных выражений [#504875 #337856]

--- src/main/java/ru/job4j/early/PassportValidator.java
+++ src/main/java/ru/job4j/early/PassportValidator.java
@@ -4,20 +4,20 @@ public class PassportValidator {
     public static boolean validate(String password) {
         boolean rsl = true;
         char tot = (char) Integer.parseInt(password);
-        if ((tot > 8 || tot < 32)
-                || Character.isUpperCase(tot)
-                || Character.isLowerCase(tot)
-                || Character.isLowerCase(tot)
-                || Character.isDigit(tot)) {
-            System.out.println("Пароль валидный");
-            return rsl;
+        if(password == null){
+            if((tot > 8) || (tot < 32)) {
+                throw new IllegalArgumentException("Ошибка при вводе пароля");
+
+            }
+            String[] temp = {"qwerty", "12345",  "password", "admin" , "user"};
+            while (String pass : temp) {
+                throw new IllegalArgumentException("Ошибка при вводе пароля");
+            }
+        }
+        boolean[] flagi = new boolean[]{Character.isLowerCase(tot), Character.isDigit(tot), Character.isUpperCase(tot)};
+        for (int i = 0; i < flagi.length ; i++) {
+
         }
         throw new IllegalArgumentException("Ошибка при вводе пароля");
     }
-
-    public static void main(String[] args) {
-        PassportValidator.validate("5");
-    }
 }
-
-
