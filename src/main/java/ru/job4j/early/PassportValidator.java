a93fb122dad18aac3a40843cb96921e7e44b8a57
marat346 <dela346@mail.ru>
2022-09-06 15:46:12 +0500

    3. final [#334073]

--- src/main/java/ru/job4j/early/PassportValidator.java
+++ src/main/java/ru/job4j/early/PassportValidator.java
@@ -4,20 +4,14 @@ public class PassportValidator {
     public static boolean validate(String password) {
         boolean rsl = true;
         char tot = (char) Integer.parseInt(password);
-        if(password == null){
-            if((tot > 8) || (tot < 32)) {
+        if (password == null) {
+            if ((tot > 8) || (tot < 32)) {
                 throw new IllegalArgumentException("Ошибка при вводе пароля");
 
             }
-            String[] temp = {"qwerty", "12345",  "password", "admin" , "user"};
-            while (String pass : temp) {
-                throw new IllegalArgumentException("Ошибка при вводе пароля");
-            }
+            String[] temp = {"qwerty", "12345", "password", "admin", "user"};
         }
         boolean[] flagi = new boolean[]{Character.isLowerCase(tot), Character.isDigit(tot), Character.isUpperCase(tot)};
-        for (int i = 0; i < flagi.length ; i++) {
-
-        }
         throw new IllegalArgumentException("Ошибка при вводе пароля");
     }
 }
